89833440d139e59c2612b349b73a9b77ab8cc1dc
Monica Mendes <monicamendesmontanha@gmail.com>
2019-07-14 21:49:07 +1000

    Setup initial

--- src/App.js
+++ src/App.js
@@ -1,24 +1,10 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <p>Eat well for a better planet!</p>
+      <p>Your diet choices can make a big difference to your personal environmental footprint, from saving water to reducing pollution and the loss of forests.</p>
     </div>
   );
 }
