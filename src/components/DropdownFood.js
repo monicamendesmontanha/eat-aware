ef3e42962655c26205151712a64b94fe2ce14bea
Monica Mendes <monicamendesmontanha@gmail.com>
2019-07-15 14:47:48 +1000

    Add dropdown food with select form react

--- src/components/DropdownFood.js
+++ src/components/DropdownFood.js
@@ -1,59 +1,24 @@
 import React, { Component } from "react";
-import { Dropdown } from "semantic-ui-react";
 import './DropdownFood.css'
 
-const foodOptions = [
-  {
-    key: "Milk (almond)",
-    text: "Milk (almond)",
-    value: "Milk (almond)",
-    type: "milk",
-    image: {  product: true, src: "../images/product/almondMilk.png" }
-  },
-  {
-    key: "Milk (dairy)",
-    text: "Milk (dairy)",
-    value: "Milk (dairy)",
-    type: "milk",
-    image: { product: true, src: "../images/product/dairyMilk.png" }
-  },
-  {
-    key: "Milk (oat)",
-    text: "Milk (oat)",
-    value: "Milk (oat)",
-    type: "milk",
-    image: { product: true, src: "../images/product/oatMilk.png" }
-  },
-  {
-    key: "Milk (rice)",
-    text: "Milk (rice)",
-    value: "Milk (rice)",
-    type: "milk",
-    image: { product: true, src: "../images/product/riceMilk.png" }
-  },
-  {
-    key: "Milk (soy)",
-    text: "Milk (soy)",
-    value: "Milk (soy)",
-    type: "milk",
-    image: { product: true, src: "../images/product/soyMilk.png" }
-  }
-];
 
 class DropdownFood extends Component {
   render() {
     return (
       <>
-        <p className="question">How do your food choices impact on the environment?</p>
-        <Dropdown
-          placeholder="Select Food"
-          fluid
-          selection
-          options={foodOptions}
-        />
-        <div>
+      <form>
+        <label>
+          <p className="question">How do your food choices impact on the environment?</p>
+        <select className="dropdownFood">
+          <option value="Milk (almond)">Milk (almond)</option>
+          <option value="Milk (dairy)">Milk (dairy)</option>
+          <option value="Milk (oat)">Milk (oat)</option>
+          <option selected value="Milk (rice)">Milk (rice)</option>
+          <option value="Milk (soy)">Milk (soy)</option>
+        </select>
+        </label>
           <input className="findOut" type="submit" value="Find Out !" />
-        </div>
+        </form>
       </>
     );
   }
