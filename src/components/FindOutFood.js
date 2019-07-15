cbd71528ebf79d3f76f7748dc1eb4161e2d5fed2
Monica Mendes <monicamendesmontanha@gmail.com>
2019-07-15 12:13:00 +1000

    Add initial style

--- src/components/FindOutFood.js
+++ src/components/FindOutFood.js
@@ -1,24 +1,22 @@
 import React, { Component } from "react";
 import food from '../images/food.png' // relative path to image
 import DropdownFood from './DropdownFood'
-
+import './FindOutFood.css'
 
 class FindOutFood extends Component {
   render() {
     return (
       <>
-        <div>
-          <img src={food} alt={"food"}/>
-          <p>
+        <div className="initialContent">
+          <img className="initialImg" src={food} alt={"food"}/>
+          <p className="initialText">
             Your diet choices can make a big difference to your personal
             environmental footprint, from saving water to reducing pollution and
             the loss of forests.
           </p>
-        </div>
-        <div>
-          <p>How do your food choices impact on the environment?</p>
+          <p className="question">How do your food choices impact on the environment?</p>
           <DropdownFood />
-          <button>Find Out!</button>
+          <button className="findOutButton">Find Out!</button>
         </div>
       </>
     );
--- src/components/Head.js
+++ src/components/Head.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import './Head.css'
 
 class Head extends Component {
  render() {
