39db7f6e3c120637e97cab3c8e7bd113e8ce9b2a
Monica Mendes <monicamendesmontanha@gmail.com>
2019-07-15 15:09:32 +1000

    Add new style

--- src/components/DropdownFood.js
+++ src/components/DropdownFood.js
@@ -6,16 +6,16 @@ class DropdownFood extends Component {
   render() {
     return (
       <>
-      <form>
+      <form className="searchFoodForm">
         <label>
           <p className="question">How do your food choices impact on the environment?</p>
-        <select className="dropdownFood">
-          <option value="Milk (almond)">Milk (almond)</option>
-          <option value="Milk (dairy)">Milk (dairy)</option>
-          <option value="Milk (oat)">Milk (oat)</option>
-          <option selected value="Milk (rice)">Milk (rice)</option>
-          <option value="Milk (soy)">Milk (soy)</option>
-        </select>
+          <select className="dropdownFood">
+            <option value="Milk (almond)">Milk (almond)</option>
+            <option value="Milk (dairy)">Milk (dairy)</option>
+            <option value="Milk (oat)">Milk (oat)</option>
+            <option selected value="Milk (rice)">Milk (rice)</option>
+            <option value="Milk (soy)">Milk (soy)</option>
+          </select>
         </label>
           <input className="findOut" type="submit" value="Find Out !" />
         </form>
