d185acdfb590a03a4e6efed90f2b62a99ffaaf93
Monica Mendes <monicamendesmontanha@gmail.com>
2019-07-15 10:09:16 +1000

    Add Head

---
+++ src/components/Head.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Head extends Component {
+ render() {
+   return (
+     <>
+       <h1>EAT AWARE</h1>
+       <h2>Eat well for a better planet!</h2>
+     </>
+   );
+ }
+}
+
+export default Head;
