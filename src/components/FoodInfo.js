da64e7b9f11d1ae820079ba38199953175f332ed
Monica Mendes <monicamendesmontanha@gmail.com>
2019-07-18 16:29:48 +1000

    Create page and add first layout version to page

--- src/components/FoodInfo.js
+++ src/components/FoodInfo.js
@@ -1,6 +1,9 @@
 import React, { Component } from "react";
 import './FoodInfo.css'
 import almondMilk from '../images/product/almondMilk.png'
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { faCarSide, faWater, faTree, faStar } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 const product = {
   name: 'Almond Milk',
@@ -12,24 +15,44 @@ const product = {
   showers: '416 showers'
 }
 
+const productName = product.name;
+const greenhouseGas = product.greenhouseEmissions;
+const petrolCar = product.petrolCar;
+const water = product.water;
+
 class FoodInfo extends Component {
   render() {
     return (
       <>
         <div className="productImgName">
           <img className="productImg" src={almondMilk} alt={"almondMilk"}/>
-          <p className="productName">{product.name}</p>
+          <p className="productName">{productName}</p>
         </div>
 
         <div className="impactDescriptionProduct">
-        <p>Contributing {product.greenhouseEmissions} to annual greenhouse gas emissions. </p>
 
-        <p>That's the equivalent of: </p>
-        <p>driving a regular petrol car {product.petrolCar}</p>
-        <p>OR</p>
-        <p>the same as heating the average UK home for {product.heating}.</p>
+        <div className="item">
+          <p className="descriptionItem">This product contributes <span className="data">{greenhouseGas}</span> to annual greenhouse gas emissions. </p>
+          <div className="itemIcon">
+            <FontAwesomeIcon icon={faTree} className="tree"/>
+          </div>
+        </div>
 
-        <p>The consumption of almond milk also uses {product.water} of water, equal to {product.showers} lasting eight minutes.</p>
+
+        <div className="item">
+          <p className="descriptionItem">That's the equivalent of: driving a regular petrol car <span className="data">{petrolCar}</span></p>
+          <div className="itemIcon">
+            <FontAwesomeIcon icon={faCarSide} className="car" />
+          </div>
+        </div>
+
+        <div className="item">
+          <p className="descriptionItem">The consumption also uses <span className="data">{water}</span> of water, equal to <span className="data">{product.showers}</span> lasting eight minutes.</p>
+          <div className="itemIcon">
+            <FontAwesomeIcon icon={faWater} className="water"/>
+          </div>
+        </div>
+
         </div>
       </>
     );
