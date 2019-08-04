19e36239aea72cede9c520e2396da8d400fffa49
Monica Mendes <monicamendesmontanha@gmail.com>
2019-08-04 17:12:07 +1000

    Add info details regarding each product on list

--- src/components/ProductDropdown.js
+++ src/components/ProductDropdown.js
@@ -2,12 +2,54 @@ import React from "react";
 import "./ProductDropdown.css";
 
 const products = [
-  "Please select a food",
-  "Milk (almond)",
-  "Milk (dairy)",
-  "Milk (oat)",
-  "Milk (rice)",
-  "Milk (soy)"
+  {
+    name: "Please select a food"
+  },
+  {
+    name: "Milk (almond)",
+    img: "../images/product/almondMilk.png",
+    greenhouseEmissions: "51 kg",
+    petrolCar: "210 km",
+    heating: "8 days",
+    water: "27,042 litres",
+    showers: "416 showers"
+  },
+  {
+    name: "Milk (dairy)",
+    img: "../images/product/dairyMilk.png",
+    greenhouseEmissions: "229 kg",
+    petrolCar: "941 km",
+    heating: "36 days",
+    water: "45,733 litres",
+    showers: "703 showers"
+  },
+  {
+    name: "Milk (oat)",
+    img: "../images/product/oatMilk.png",
+    greenhouseEmissions: "65 kg",
+    petrolCar: "270 km",
+    heating: "10 days",
+    water: "3,512 litres",
+    showers: "54 showers"
+  },
+  {
+    name: "Milk (rice)",
+    img: "../images/product/riceMilk.png",
+    greenhouseEmissions: "86 kg",
+    petrolCar: "353 km",
+    heating: "13 days",
+    water: "19,642 litres",
+    showers: "302 showers"
+  },
+  {
+    name: "Milk (soy)",
+    img: "../images/product/soyMilk.png",
+    greenhouseEmissions: "71 kg",
+    petrolCar: "293 km",
+    heating: "11 days",
+    water: "9,500 litres",
+    showers: "146 showers"
+  }
 ];
 
 const ProductDropdown = props => {
@@ -24,7 +66,7 @@ const ProductDropdown = props => {
             onChange={props.handleChange}
           >
             {
-              products.map((product) => <option value={product}>{product}</option>)
+              products.map((product) => <option value={product.name}>{product.name}</option>)
             }
           </select>
         </label>
