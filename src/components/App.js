b84a0529e645d6262b76608733abf3e3cf19fa4a
Monica Mendes <monicamendesmontanha@gmail.com>
2019-08-04 18:31:46 +1000

    Extract products from App.js and create a new file

--- src/components/App.js
+++ src/components/App.js
@@ -3,57 +3,7 @@ import Head from "./Head";
 import InitialContainer from "./InitialContainer";
 import ProductDropdown from "./ProductDropdown";
 import ProductInfo from "./ProductInfo";
-
-const products = [
-  {
-    name: "Please select a food"
-  },
-  {
-    name: "Milk (almond)",
-    img: "products/almondMilk.png",
-    greenhouseEmissions: "51 kg",
-    petrolCar: "210 km",
-    heating: "8 days",
-    water: "27,042 litres",
-    showers: "416 showers"
-  },
-  {
-    name: "Milk (dairy)",
-    img: "products/dairyMilk.png",
-    greenhouseEmissions: "229 kg",
-    petrolCar: "941 km",
-    heating: "36 days",
-    water: "45,733 litres",
-    showers: "703 showers"
-  },
-  {
-    name: "Milk (oat)",
-    img: "products/oatMilk.png",
-    greenhouseEmissions: "65 kg",
-    petrolCar: "270 km",
-    heating: "10 days",
-    water: "3,512 litres",
-    showers: "54 showers"
-  },
-  {
-    name: "Milk (rice)",
-    img: "products/riceMilk.png",
-    greenhouseEmissions: "86 kg",
-    petrolCar: "353 km",
-    heating: "13 days",
-    water: "19,642 litres",
-    showers: "302 showers"
-  },
-  {
-    name: "Milk (soy)",
-    img: "products/soyMilk.png",
-    greenhouseEmissions: "71 kg",
-    petrolCar: "293 km",
-    heating: "11 days",
-    water: "9,500 litres",
-    showers: "146 showers"
-  }
-];
+import products from "./products";
 
 class App extends Component {
   constructor() {
