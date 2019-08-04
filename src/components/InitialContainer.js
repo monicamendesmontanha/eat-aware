5f4b2fcd72ef884e2c03a12b4c6006f75ef598f9
Monica Mendes <monicamendesmontanha@gmail.com>
2019-08-04 18:07:41 +1000

    Cleanup code

--- src/components/InitialContainer.js
+++ src/components/InitialContainer.js
@@ -1,14 +1,13 @@
 import React, { Component } from "react";
-import worldFood from '../images/World-Food.png'
-// import food from '../images/food.png' // relative path to image
-import './InitialContainer.css'
+import worldFood from "../images/World-Food.png";
+import "./InitialContainer.css";
 
 class InitialContainer extends Component {
   render() {
     return (
       <>
         <div className="initialContent">
-          <img className="initialImg" src={worldFood} alt={"worldFood"}/>
+          <img className="initialImg" src={worldFood} alt={"worldFood"} />
           <p className="initialText">
             Your diet choices can make a big difference to your personal
             environmental footprint, from saving water to reducing pollution and
